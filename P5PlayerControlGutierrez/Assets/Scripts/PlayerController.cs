3a346b88c44a773725809ab0cbe4adee445e883d
nataliag409 <natalia212329@gmail.com>
2024-02-23 21:54:32 -0800

    I finished 1.4 and just need to do the bonus.
    1) added more obsticls
    2) made the car move
    3)added a turn speed.
    4) Made the camera follow the car properly.

--- P5PlayerControlGutierrez/Assets/Scripts/PlayerController.cs
+++ P5PlayerControlGutierrez/Assets/Scripts/PlayerController.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //Private Vareabls
+    private float speed = 10.0f;
+    private float turnSpeed = 25.0f;
+    private float horizontalInput;
+    private float forwardInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +20,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //This is where we get player input
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
         // Move the vehicle forward 
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //We tuen the vehicle
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
