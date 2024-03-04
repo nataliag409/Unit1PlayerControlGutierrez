c4ff23dbd19830abe3316ed82ab9d859f3bdb6b1
nataliag409 <natalia212329@gmail.com>
2024-03-04 13:48:48 -0800

    Finishde mt project
    Finishde mt project

--- P5PlayerControlGutierrez/Assets/Scripts/PlayerController.cs
+++ P5PlayerControlGutierrez/Assets/Scripts/PlayerController.cs
@@ -10,6 +10,9 @@ public class PlayerController : MonoBehaviour
     private float turnSpeed = 25.0f;
     private float horizontalInput;
     private float forwardInput;
+    public Camera mainCamera;
+    public Camera hoodCamera;
+    public KeyCode switchKey;
 
     // Start is called before the first frame update
     void Start()
@@ -28,5 +31,11 @@ public class PlayerController : MonoBehaviour
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         //We tuen the vehicle
         transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+
+        if (Input.GetKeyDown(switchKey))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            hoodCamera.enabled = !hoodCamera.enabled;
+        }
     }
 }
